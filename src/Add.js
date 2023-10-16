eba8f1d04ee63c94c8d978a29346d655550715b5
Yong Minyue <minyue.yong@gswhouse.com>
2023-10-16 13:37:18 +0800

    initial commit

---
+++ src/Add.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+export default function Add( {addHabits, addSpending} ) {
+	const [activityName, setActivityName] = useState("");
+	const [points, setPoints] = useState("");
+	const [activityType, setActivityType] = useState("habit");
+	
+	function handleSubmit(e) {
+		
+		e.preventDefault();
+		
+			
+		if (activityType === "habit") {
+			
+		  addHabits(activityName, points);
+		  
+		} else if (activityType === "spending") {
+			
+		  addSpending(activityName, points);
+		}
+
+		
+	}
+	
+  return (
+	  <form method="post" onSubmit={handleSubmit} className="add">
+			
+			
+				<div className="addoptions">
+					Activity Name : <input type="text" name="activity" value={activityName}  onChange={(e) => setActivityName(e.target.value)} /> <br/>
+					<label for="type">Activity Type&nbsp;&nbsp;&nbsp;&nbsp;:</label>
+
+				
+					<select name="type" id="type" value={activityType} onChange={(e) => setActivityType(e.target.value)}>
+					  <option value="habit">Habit</option>
+					  <option value="spending">Spending</option>
+					</select>
+					
+					<br/>
+					Points   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  : <input type="number" name="points" min="0" value={points}  onChange={(e) => setPoints(e.target.value)}/> <br/>
+				</div>
+				
+				<div className="button1">
+					<button type="submit">
+					  Add New Habits/Spending
+					</button>
+				</div>
+			
+		</form>
+  );
+  
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import Add from './Add.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function HabitTracker() {
+	 
+	  const [points, setPoints] = useState(0);
+	  const [spending, setSpending] = useState([]);
+	  const [habits, setHabits] = useState([]);
+
+	  function addPoints( i ) {
+		setPoints(points + i);
+	  }
+
+	 function spendPoints( i ) {
+		setPoints(points - i);
+	  }
+	  
+	 function addHabits ( newHabitName, newPoints){
+		  
+		 const newHabit = {
+			Habit: newHabitName,
+			Points: newPoints,
+		  };
+
+		  // create a copy of habits array and assign the newHabit into it and set inside the habits array
+		  setHabits([...habits, newHabit]);
+	  }
+	  
+	  function addSpending (newSpendingName, newPoints ) {
+		 
+		   const newSpending = {
+			Spending: newSpendingName,
+			Points: newPoints,
+		  };
+
+		  // create a copy of spending array and assign the newSpending into it and set inside the spending array
+		  setSpending([...spending, newSpending]);
+		  
+	  }
+	  
+	  
+	  return (
+		<div className="App">
+		  <p><b>Habit Tracker</b></p>
+		  <p>Your Points: {points}</p>
+
+		  <Add addHabits={addHabits} addSpending={addSpending} />
+		  <br/>
+
+		  <div className="habits" >
+			   <p> Habits </p>
+			   <Habits  habitList={habits} adPoints={addPoints}  />
+		  </div>
+		   <br/>
+			
+		  <div className="spending" >	
+			   <p>Spending</p>
+			   <Spending spendingList={spending} spendPoints={spendPoints}  />
+		  </div>
+		</div>
+	  );
+}
+/* adPoints is pass as variable */
+function Habits({habitList, adPoints, addHabits}) {
+
+
+  const moves = habitList.map((squares, index) => (
+   
+      <button onClick={() => adPoints(parseInt(squares.Points))} className="button1">
+        <div style={{ marginRight: '10px' }}>{squares.Habit}</div>
+        <div style={{ marginLeft: '10px' }}>{squares.Points} points</div>
+      </button>
+    
+   
+  ));
+
+  return <>{moves}</>;
 }
 
-export default App;
+
+/* adPoints is pass as variable */
+function Spending({spendingList, spendPoints}) {
+
+
+  const moves = spendingList.map((squares, index) => (
+   
+      <button onClick={() => spendPoints(parseInt(squares.Points))} className="button1">
+        <div style={{ marginRight: '10px' }}>{squares.Spending}</div>
+        <div style={{ marginLeft: '10px' }}>{squares.Points} points</div>
+      </button>
+    
+   
+  ));
+
+  return <>{moves}</>;
+}
+
+
+
